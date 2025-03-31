ab8f7afe0c2ff601d60dc18f18ad37cf57326494
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2025-03-31 13:04:42 +0200

    update page title to remove unnecessary prefix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'LINK | Coding + Design',
+  title: 'Coding + Design',
   description: 'Portfolio',
 };
 
